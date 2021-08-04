d74ba6be79c715c465633c806daf5b06d2b0d13d
Troy Dorman <troydorman@gmail.com>
2021-08-04 07:39:33 -0600

    added ability to write custom data into the database with json in the post body

--- controllers/productController.js
+++ controllers/productController.js
@@ -34,15 +34,24 @@ async function getProduct(req, res, id) {
 // @ route POST /api/products
 async function createProduct(req, res) {
   try {
-    const product = {
-      title: "Test Product",
-      description: "This is my product",
-      price: 100,
-    };
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
 
-    const newProduct = await Product.create(product);
-    res.writeHead(201, { "Content-Type": "application/json" });
-    return res.end(JSON.stringify(newProduct));
+    req.on("end", async () => {
+      const { title, description, price } = JSON.parse(body);
+
+      const product = {
+        title,
+        description,
+        price,
+      };
+
+      const newProduct = await Product.create(product);
+      res.writeHead(201, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(newProduct));
+    });
   } catch (error) {
     console.log(error);
   }
