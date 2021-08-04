ef9a4dc45725d7eff1bc7de63d4529da7f80f45c
Troy Dorman <troydorman@gmail.com>
2021-08-04 07:05:55 -0600

    created else to the conditional to give a 404 response to any route besides /api/products

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const server = http.createServer((req, res) => {
   if (req.url === "/api/products") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(products));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found." }));
   }
 });
 
