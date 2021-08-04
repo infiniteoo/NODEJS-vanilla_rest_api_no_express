e98b9f35513845f108fb56df08e89c089d008dd5
Troy Dorman <troydorman@gmail.com>
2021-08-04 08:03:20 -0600

    added ability to update an already-existing product in the json database wit a put request and custom data in the request body

--- controllers/productController.js
+++ controllers/productController.js
@@ -49,7 +49,36 @@ async function createProduct(req, res) {
 
     res.writeHead(201, { "Content-Type": "application/json" });
     return res.end(JSON.stringify(newProduct));
-    
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// @desc - update a product
+// @ route PUT /api/products/:id
+async function updateProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      const body = await getPostData(req);
+
+      const { title, description, price } = JSON.parse(body);
+
+      const productData = {
+        title: title || product.title,
+        description: description || product.description,
+        price: price || product.price,
+      };
+
+      const updProduct = await Product.update(id, productData);
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(updProduct));
+    }
   } catch (error) {
     console.log(error);
   }
@@ -59,4 +88,5 @@ module.exports = {
   getProducts,
   getProduct,
   createProduct,
+  updateProduct
 };
