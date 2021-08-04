f3a140d5bdccda26897775d1c6c9e17cea897cc8
Troy Dorman <troydorman@gmail.com>
2021-08-04 09:21:32 -0600

    finished CRUD ability with server

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,4 @@
-const products = require("../data/products.json");
+let products = require("../data/products.json");
 const { v4: uuidv4 } = require("uuid");
 const { writeDataToFile } = require("../utils");
 
@@ -34,9 +34,18 @@ function update(id, product) {
   });
 }
 
+function remove(id) {
+  return new Promise((resolve, reject) => {
+    products = products.filter((p) => p.id !== id);
+    writeDataToFile("./data/products.json", products);
+    resolve();
+  });
+}
+
 module.exports = {
   findAll,
   findById,
   create,
   update,
+  remove,
 };
